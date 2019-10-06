edca9de8576de2a9740b764266814da71cc7126a
Fábián Gábor <info@fabiangabor.com>
2019-10-06 16:55:15 +0200

    Update main.c

--- main.c
+++ main.c
@@ -5,7 +5,7 @@
 #include <pthread.h>
 #include <math.h>
 #include <windows.h>
-#define LIMIT 100
+#define LIMIT 100000
 
 int is_prime(unsigned long int x) {
     //if (x <= 1) return 0; // zero and one are not prime
@@ -16,10 +16,8 @@ int is_prime(unsigned long int x) {
     return 1;
 }
 
-int print_prime (unsigned long int i, unsigned long int *out) {
+void print_prime (unsigned long int i, unsigned long int *out) {
     printf("%lu ",out[i]);
-    //printf("%lu ", i);
-    return 0;
 }
 
 int main() {
@@ -40,41 +38,65 @@ int main() {
     #pragma omp parallel
     {
         #pragma omp for schedule (runtime)
-        for (unsigned long int i=3; i<=limit; i += 2) {
-            if (is_prime(i)) {
-                out[(int)floor(i/2)] = i;
-                #pragma omp critical
-                prime_count++;
+            for (unsigned long int i=3; i<=limit; i += 2) {
+                if (is_prime(i)) {
+                    out[(int)floor(i/2)] = i;
+                    #pragma omp critical
+                        prime_count++;
+                }
+                else {
+                    out[(int)floor(i/2)]=0;
+                }
             }
-            else {
-                out[(int)floor(i/2)]=0;
-            }
-        }
     }
     endCalc = omp_get_wtime();
     runTime = endCalc - startCalc;
 
-    for (int i=0; i<floor(limit/2); i++) {
+    printf("Calculated all %lu prime numbers under %lu in %g seconds\n\n",prime_count, limit, runTime);
+
+
+/*
+ * // This is painfully slow
+    int nullCount = 0;
+    int newLimit = floor(limit/2);
+    for (int i=0; i<newLimit; i++) {
+        nullCount=0;
         if (out[i]==0) {
-            for (int j=i; j<floor(limit/2); j++)
-                out[j]=out[j+1];
-            limit-=2;
+            int j = i;
+            while (out[j]==0) {
+                nullCount++;
+                j++;
+            }
+
+            for (int j=i; j<floor(limit/2); j++) {
+                out[j]=out[j+nullCount];
+                out[j+nullCount]=0;
+            }
+            newLimit-=nullCount;
         }
     }
 
-    double startPrint = omp_get_wtime();
     printf("Primes:\t");
-    for (int k=0; k<floor(limit/2); k++) {
-        printf("%lu\t",out[k]);
+    for (int i=0; i<newLimit; i++) {
+        printf("%lu\t",out[i]);
     }
+*/
+    char yesno = 'n';
+    printf("Do you want to print the primes up to %lu? (y/n) ", limit);
+    scanf("%c", &yesno);
+    if ((yesno == 'y') || (yesno == 'Y')) {
+        double startPrint = omp_get_wtime();
+        for (unsigned long i=0; i<=floor(limit/2); i++){
+            if (out[i]!=0) printf("%lu ",out[i]);
+        }
 
-    double endPrint = omp_get_wtime();
-    double printTime = endPrint - startPrint;
+        double endPrint = omp_get_wtime();
+        double printTime = endPrint - startPrint;
 
-
-    printf("\n\nCalculated all %lu prime numbers under %lu in %g seconds\n\n",prime_count, LIMIT, runTime);
-    printf("\n\nPrinting all %lu prime numbers under %lu took %g seconds to print\n\n",prime_count, LIMIT, printTime);
-
+        printf("\n\nCalculated all %lu prime numbers under %lu in %g seconds\n",prime_count, limit, runTime);
+        printf("Printing all %lu prime numbers under %lu took %g seconds to print\n\n\n",prime_count, limit, printTime);
+    }
+    printf("\n");
 
     return 0;
 }
